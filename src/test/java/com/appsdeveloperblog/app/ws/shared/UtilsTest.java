f3d51410e81a59787b03e0bf73d1437cfa9f795a
Sergey Kargopolov <skargopolov@central1.com>
2018-08-10 08:16:50 -0700

    Added JUnit Integration test

---
+++ src/test/java/com/appsdeveloperblog/app/ws/shared/UtilsTest.java
@@ -0,0 +1,44 @@
+package com.appsdeveloperblog.app.ws.shared;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UtilsTest {
+	
+	@Autowired
+	Utils utils;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	final void testGenerateUserId() {
+		String userId = utils.generateUserId(30);
+		String userId2 = utils.generateUserId(30);
+		
+		assertNotNull(userId);
+		assertNotNull(userId2);
+		
+		assertTrue(userId.length() == 30);
+		assertTrue( !userId.equalsIgnoreCase(userId2) );
+	}
+
+	@Test
+	@Disabled
+	final void testHasTokenExpired() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/appsdeveloperblog/app/ws/ui/controller/UserControllerTest.java
@@ -0,0 +1,87 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.appsdeveloperblog.app.ws.service.impl.UserServiceImpl;
+import com.appsdeveloperblog.app.ws.shared.dto.AddressDTO;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+import com.appsdeveloperblog.app.ws.ui.contoller.UserController;
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
+
+class UserControllerTest {
+
+	@InjectMocks
+	UserController userController;
+	
+	@Mock
+	UserServiceImpl userService;
+	
+	UserDto userDto;
+	
+	final String USER_ID = "bfhry47fhdjd7463gdh";
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		
+		userDto = new UserDto();
+        userDto.setFirstName("Sergey");
+        userDto.setLastName("Kargopolov");
+        userDto.setEmail("test@test.com");
+        userDto.setEmailVerificationStatus(Boolean.FALSE);
+        userDto.setEmailVerificationToken(null);
+        userDto.setUserId(USER_ID);
+        userDto.setAddresses(getAddressesDto());
+        userDto.setEncryptedPassword("xcf58tugh47");
+		
+	}
+
+	@Test
+	final void testGetUser() {
+	    when(userService.getUserByUserId(anyString())).thenReturn(userDto);	
+	    
+	    UserRest userRest = userController.getUser(USER_ID);
+	    
+	    assertNotNull(userRest);
+	    assertEquals(USER_ID, userRest.getUserId());
+	    assertEquals(userDto.getFirstName(), userRest.getFirstName());
+	    assertEquals(userDto.getLastName(), userRest.getLastName());
+	    assertTrue(userDto.getAddresses().size() == userRest.getAddresses().size());
+	}
+	
+	
+	private List<AddressDTO> getAddressesDto() {
+		AddressDTO addressDto = new AddressDTO();
+		addressDto.setType("shipping");
+		addressDto.setCity("Vancouver");
+		addressDto.setCountry("Canada");
+		addressDto.setPostalCode("ABC123");
+		addressDto.setStreetName("123 Street name");
+
+		AddressDTO billingAddressDto = new AddressDTO();
+		billingAddressDto.setType("billling");
+		billingAddressDto.setCity("Vancouver");
+		billingAddressDto.setCountry("Canada");
+		billingAddressDto.setPostalCode("ABC123");
+		billingAddressDto.setStreetName("123 Street name");
+
+		List<AddressDTO> addresses = new ArrayList<>();
+		addresses.add(addressDto);
+		addresses.add(billingAddressDto);
+
+		return addresses;
+
+	}
+
+}
