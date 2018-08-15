045d2c0591220d71d6b1d2a461e4418b73d78163
Sergey Kargopolov <skargopolov@central1.com>
2018-08-15 07:26:10 -0700

    Testing if the JWT Token Has Expired

--- src/main/java/com/appsdeveloperblog/app/ws/shared/Utils.java
+++ src/main/java/com/appsdeveloperblog/app/ws/shared/Utils.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.appsdeveloperblog.app.ws.security.SecurityConstants;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -36,18 +37,25 @@ public class Utils {
 
         return new String(returnValue);
     }
-    
-    public static boolean hasTokenExpired(String token) {
-        Claims claims = Jwts.parser()         
-       .setSigningKey( SecurityConstants.getTokenSecret() )
-       .parseClaimsJws( token ).getBody();
- 
-        Date tokenExpirationDate = claims.getExpiration();
-        Date todayDate = new Date();
- 
-        return tokenExpirationDate.before(todayDate); 
-    }
-    
+
+	public static boolean hasTokenExpired(String token) {
+		boolean returnValue = false;
+
+		try {
+			Claims claims = Jwts.parser().setSigningKey(SecurityConstants.getTokenSecret()).parseClaimsJws(token)
+					.getBody();
+
+			Date tokenExpirationDate = claims.getExpiration();
+			Date todayDate = new Date();
+
+			returnValue = tokenExpirationDate.before(todayDate);
+		} catch (ExpiredJwtException ex) {
+			returnValue = true;
+		}
+
+		return returnValue;
+	}
+
     public String generateEmailVerificationToken(String userId) {
         String token = Jwts.builder()
                 .setSubject(userId)
--- src/test/java/com/appsdeveloperblog/app/ws/shared/UtilsTest.java
+++ src/test/java/com/appsdeveloperblog/app/ws/shared/UtilsTest.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog.app.ws.shared;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +12,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import io.jsonwebtoken.SignatureException;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -34,11 +39,23 @@ class UtilsTest {
 		assertTrue(userId.length() == 30);
 		assertTrue( !userId.equalsIgnoreCase(userId2) );
 	}
-
+	
 	@Test
-	@Disabled
-	final void testHasTokenExpired() {
-		fail("Not yet implemented");
+	final void testHasTokenNotExpired() {
+		String token = utils.generateEmailVerificationToken("4yr65hhyid84");
+		assertNotNull(token);
+		
+		boolean hasTokenExpired = Utils.hasTokenExpired(token);
+		assertFalse(hasTokenExpired);
 	}
-
+	
+	@Test
+	final void testHasTokenExpired()
+	{
+		String expiredToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MUB0ZXN0LmNvbSIsImV4cCI6MTUzMjc3Nzc3NX0.cdudUo3pwZLN9UiTuXiT7itpaQs6BgUPU0yWbNcz56-l1Z0476N3H_qSEHXQI5lUfaK2ePtTWJfROmf0213UJA";
+		boolean hasTokenExpired = Utils.hasTokenExpired(expiredToken);
+		
+		assertTrue(hasTokenExpired);
+	}
+	
 }
