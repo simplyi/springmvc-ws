21543833df410b1f9d22608f53d8a82171947608
mac <diggerworker@mail.ru>
2019-03-10 18:03:36 +0300

    Tests fix

--- src/main/java/com/appsdeveloperblog/app/ws/io/repository/UserRepository.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/repository/UserRepository.java
@@ -19,21 +19,21 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
 	UserEntity findByUserId(String userId);
 	UserEntity findUserByEmailVerificationToken(String token); 
 	
-	@Query(value="select * from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'", 
-			countQuery="select count(*) from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'", 
+	@Query(value="select * from users u where u.EMAIL_VERIFICATION_STATUS = 'true'", 
+			countQuery="select count(*) from users u where u.EMAIL_VERIFICATION_STATUS = 'true'", 
 			nativeQuery = true)
 	Page<UserEntity> findAllUsersWithConfirmedEmailAddress( Pageable pageableRequest );
 	
-	@Query(value="select * from Users u where u.first_name = ?1",nativeQuery=true)
+	@Query(value="select * from users u where u.first_name = ?1",nativeQuery=true)
 	List<UserEntity> findUserByFirstName(String firstName);
 	
-	@Query(value="select * from Users u where u.last_name = :lastName",nativeQuery=true)
+	@Query(value="select * from users u where u.last_name = :lastName",nativeQuery=true)
 	List<UserEntity> findUserByLastName(@Param("lastName") String lastName);
 	
-	@Query(value="select * from Users u where first_name LIKE %:keyword% or last_name LIKE %:keyword%",nativeQuery=true)
+	@Query(value="select * from users u where first_name LIKE %:keyword% or last_name LIKE %:keyword%",nativeQuery=true)
 	List<UserEntity> findUsersByKeyword(@Param("keyword") String keyword);
 	
-	@Query(value="select u.first_name, u.last_name from Users u where u.first_name LIKE %:keyword% or u.last_name LIKE %:keyword%",nativeQuery=true)
+	@Query(value="select u.first_name, u.last_name from users u where u.first_name LIKE %:keyword% or u.last_name LIKE %:keyword%",nativeQuery=true)
 	List<Object[]> findUserFirstNameAndLastNameByKeyword(@Param("keyword") String keyword);
 	
 	@Transactional
