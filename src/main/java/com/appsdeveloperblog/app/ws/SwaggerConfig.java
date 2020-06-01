158495d176134fb9028288fa3cb70ddc7a04f4ae
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-06-01 09:36:15 -0400

    Update SwaggerConfig file with LinkDiscoverers Bean

---
+++ src/main/java/com/appsdeveloperblog/app/ws/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.appsdeveloperblog.app.ws;
+ 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+   @Bean
+   public Docket apiDocket() {
+       
+       Docket docket =  new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.appsdeveloperblog.app.ws"))
+                .paths(PathSelectors.any())
+                .build();
+       
+       return docket;
+       
+    } 
+   
+	@Bean
+	public LinkDiscoverers discovers() {
+		
+		List<LinkDiscoverer> plugins = new ArrayList<>();
+		plugins.add(new CollectionJsonLinkDiscoverer());
+		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+		
+	}
+
+
+
+}
