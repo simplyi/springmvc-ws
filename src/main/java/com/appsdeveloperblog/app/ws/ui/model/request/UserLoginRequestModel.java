4ccd6f2a04a41a255aef61e22bce486bcab85162
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-04-07 16:22:58 -0400

    Created UserLoginRequestModel

---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserLoginRequestModel.java
@@ -0,0 +1,22 @@
+package com.appsdeveloperblog.app.ws.ui.model.request;
+
+public class UserLoginRequestModel {
+	private String email;
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
