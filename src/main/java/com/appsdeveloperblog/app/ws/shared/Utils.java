6bb40ccf115e0baaece9d62217d1b5d791b7e893
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-12-11 09:28:57 -0500

    Updated Spring Boot version to 3.2.0

--- src/main/java/com/appsdeveloperblog/app/ws/shared/Utils.java
+++ src/main/java/com/appsdeveloperblog/app/ws/shared/Utils.java
@@ -1,8 +1,11 @@
 package com.appsdeveloperblog.app.ws.shared;
 
 import java.security.SecureRandom;
+import java.time.Instant;
 import java.util.Date;
 import java.util.Random;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.stereotype.Service;
 
@@ -10,23 +13,24 @@ import com.appsdeveloperblog.app.ws.security.SecurityConstants;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class Utils {
 
     private final Random RANDOM = new SecureRandom();
     private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	
+
     public String generateUserId(int length) {
         return generateRandomString(length);
     }
-    
+
     public String generateAddressId(int length) {
         return generateRandomString(length);
     }
-    
+
     private String generateRandomString(int length) {
         StringBuilder returnValue = new StringBuilder(length);
 
@@ -37,40 +41,47 @@ public class Utils {
         return new String(returnValue);
     }
 
-	public static boolean hasTokenExpired(String token) {
-		boolean returnValue = false;
+    public static boolean hasTokenExpired(String token) {
+        boolean returnValue = false;
 
-		try {
-			Claims claims = Jwts.parser().setSigningKey(SecurityConstants.getTokenSecret()).parseClaimsJws(token)
-					.getBody();
+        try {
 
-			Date tokenExpirationDate = claims.getExpiration();
-			Date todayDate = new Date();
+            byte[] secretKeyBytes = SecurityConstants.getTokenSecret().getBytes();
+            SecretKey key = Keys.hmacShaKeyFor(secretKeyBytes);
 
-			returnValue = tokenExpirationDate.before(todayDate);
-		} catch (ExpiredJwtException ex) {
-			returnValue = true;
-		}
+            JwtParser parser = Jwts.parser().verifyWith(key).build();
 
-		return returnValue;
-	}
+            Claims claims = parser.parseSignedClaims(token).getPayload();
+
+            Date tokenExpirationDate = claims.getExpiration();
+            Date todayDate = new Date();
+
+            returnValue = tokenExpirationDate.before(todayDate);
+        } catch (ExpiredJwtException ex) {
+            returnValue = true;
+        }
+
+        return returnValue;
+    }
 
     public String generateEmailVerificationToken(String userId) {
-        String token = Jwts.builder()
-                .setSubject(userId)
-                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.getTokenSecret())
-                .compact();
+        return generateToken(userId);
+    }
+
+    public String generatePasswordResetToken(String userId) {
+        return generateToken(userId);
+    }
+
+    private String generateToken(String userId) {
+        byte[] secretKeyBytes = SecurityConstants.getTokenSecret().getBytes();
+        SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyBytes);
+        Instant now = Instant.now();
+
+        String token = Jwts.builder().subject(userId)
+                .expiration(Date.from(now.plusMillis(SecurityConstants.EXPIRATION_TIME))).issuedAt(Date.from(now))
+                .signWith(secretKey).compact();
+
         return token;
     }
-    
-    public String generatePasswordResetToken(String userId)
-    {
-        String token = Jwts.builder()
-                .setSubject(userId)
-                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.getTokenSecret())
-                .compact();
-        return token;
-    }
+
 }
