8da18156ca2cfee9e24331535473af91f621fbaa
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2025-03-14 11:02:05 -0400

    Updated SessionManagement to make application Stateless.

--- src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.appsdeveloperblog.app.ws.service.UserService;
 import org.springframework.context.annotation.Configuration;
@@ -41,7 +42,9 @@ public class WebSecurity {
 						.anyRequest().authenticated())
 				.authenticationManager(authenticationManager)
 				.addFilter(authenticationFilter)
-				.addFilter(new AuthorizationFilter(authenticationManager));
+				.addFilter(new AuthorizationFilter(authenticationManager))
+				.sessionManagement(session -> session
+	                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 		return http.build();
 	}
